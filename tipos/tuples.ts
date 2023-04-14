044f330b55982d32a923483287a12f0d103d9b92
Alexis Montilla <alexisamm9261@gmail.com>
2023-04-14 18:35:19 -0400

    feature: interfaces, decorators, generics added

--- tipos/tuples.ts
+++ tipos/tuples.ts
@@ -5,7 +5,16 @@
     hero[0] = 'Ironman';
     hero[1] = 50;
     hero[2] = false;
-
     console.log(hero)
+
+    const villian: [string, number] = ['Dr Strange', 100];
+
+    villian[0] = 'Magneto';
+    villian[1] = 26;
+
+
+    // Defino un tipo de dato que sera una tupla que recibe un string, un numero y luego una cantidad no definida de bolleanos
+    type StringNumberBooleans = [string, number, ...boolean[]];
+    const c: StringNumberBooleans = ["world", 3, true, false, true, false, true];
 
 })()
